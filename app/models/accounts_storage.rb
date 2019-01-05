77681fc698a0380147f7efb833461c98a3365249
Filip Andonovski <filip@coqtail.com>
2019-01-05 12:06:16 +0100

    AccountsStorage class added for initial data persistence, removed test_data initializer

---
+++ app/models/accounts_storage.rb
@@ -0,0 +1,25 @@
+require 'singleton'
+class AccountsStorage
+  include Singleton
+
+  def initialize
+    accounts_data = [{number: "NL323232", balance: 200, customer_id: 1},
+                     {number: "NL323262", balance: 1460, customer_id: 2},
+                     {number: "NL323242", balance: 3240, customer_id: 3},
+                     {number: "NL323249", balance: 2100, customer_id: 1}]
+    @accounts = accounts_data.map do |account_hash|
+      Account.new(account_hash)
+    end
+  end
+
+  def create_customer_account!(customer_id, initial_balance)
+    new_account = Account.new({balance: initial_balance, customer_id: customer_id})
+    @accounts << new_account
+    new_account
+  end
+
+  def get_customer_accounts(customer_id)
+    @accounts.select {|account| account.customer_id == customer_id}
+  end
+
+end
